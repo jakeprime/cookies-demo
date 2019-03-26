02e6b2f5969f26d0f240a95bff357f686852c0f6
Jake Prime <jake.prime@gmail.com>
2019-03-26 17:43:07 +0000

    :sparkles: Can write new set-cookie headers

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,7 +1,15 @@
 class CookiesController < ApplicationController
   def index
+    headers['Set-Cookie'] = set_cookies
     @cookie_headers = headers['Set-Cookie']
-    @cookies = cookies.each
+    @cookies = cookies
     @raw_cookies = request.headers['Cookie']
+  end
+
+  private
+
+  def set_cookies
+    return [] unless params[:cookies]
+    params[:cookies].split("\n").map(&:strip)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'cookies#index'
-  resources :cookies, only: %i[index update]
+  post '/' => 'cookies#index'
 end
